7004ac2b667cbb635b2d7adadecebb46620a0dee
Zainab Nisa <nisazainab53@gmail.com>
2023-02-18 22:45:54 +0500

    js project

---
+++ index.js
@@ -0,0 +1,42 @@
+
+let oneO=document.getElementById("one-ch")
+let twoT=document.getElementById("two-ch")
+let thrT=document.getElementById("thr-ch")
+let fourF=document.getElementById("four-ch")
+let fiveF=document.getElementById("five-ch")
+let sixS=document.getElementById("six-ch")
+let sevenS=document.getElementById("seven-ch")
+let eightE=document.getElementById("eight-ch")
+let nineN=document.getElementById("nine-ch")
+let tenT=document.getElementById("ten-ch")
+function one(){
+  oneO.textContent="One"
+}
+function two(){
+    twoT.textContent="Two"
+}
+function thr(){
+   thrT.textContent="Three"
+}
+function four(){
+    fourF.textContent="Four"
+}
+function five(){
+    fiveF.textContent="Five"
+}
+function six(){
+    sixS.textContent="Six"
+}
+function seven(){
+    sevenS.textContent="Seven"
+}
+function eight(){
+    eightE.textContent="Eight"
+}
+function nine(){
+    nineN.textContent="Nine"
+}
+function ten(){
+    tenT.textContent="Ten"
+}
+
